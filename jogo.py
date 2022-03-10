d4b882f212a13fc8a584dda8cf5e5d1f050f55c5
Dcorder123 <101361658+Dcorder123@users.noreply.github.com>
2022-03-10 16:51:53 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,201 @@
+from constantes import *
+
+# contador
+liberdades = []
+bloco = []
+
+#contar liberdades e salvar coordenadas das peças em grupo
+def contar_liberdades(tab):
+    count = 8
+    count2 = 8
+    for linha in range(len(tab)):
+        for coluna in range(len(tab)+1):
+
+            if tab[linha][coluna] == BOLA_PRETA or tab[linha][coluna] == BOLA_BRANCA:
+
+                if tab[linha][coluna] == BOLA_PRETA:
+
+                    if tab[linha - 1][coluna] == BOLA_BRANCA or linha == 0:
+                        count -= 1
+
+                    else:
+                        if tab[linha - 1][coluna] == BOLA_PRETA:
+                            count += 1
+                        else:
+                            tab[linha - 1][coluna] = "x"
+
+                    if linha == len(tab) - 1:
+                        count -= 1
+                    else:
+                        if tab[linha + 1][coluna] == BOLA_PRETA or linha == len(tab) - 1:
+                            count -= 1
+
+                        else:
+                            if tab[linha + 1][coluna] == BOLA_PRETA:
+                                count += 1
+                            else:
+                                tab[linha + 1][coluna] = "x"
+
+                    if tab[linha][coluna - 1] == BOLA_BRANCA or coluna == 1:
+                        count -= 1
+
+                    else:
+                        if tab[linha][coluna - 1] == BOLA_PRETA:
+                            count += 1
+                        else:
+                            tab[linha][coluna - 1] = "x"
+
+                    if tab[linha][coluna + 1] == BOLA_BRANCA or coluna == len(tab):
+                        count -= 1
+
+                    else:
+                        if tab[linha][coluna + 1] == BOLA_PRETA:
+                            count += 1
+                        else:
+                            tab[linha][coluna + 1] = "x"
+
+                else:
+                    if tab[linha - 1][coluna] == BOLA_PRETA or linha == 0:
+                        count2 -= 1
+
+                    else:
+                        if tab[linha - 1][coluna] == BOLA_BRANCA:
+                            count2 += 1
+                        else:
+                            tab[linha - 1][coluna] = "Y"
+
+                    if linha == len(tab)-1:
+                        count2 -=1
+                    else:
+                        if tab[linha + 1][coluna] == BOLA_PRETA or linha == len(tab) - 1:
+                            count2 -= 1
+
+                        else:
+                            if tab[linha + 1][coluna] == BOLA_BRANCA:
+                                count2 += 1
+                            else:
+                                tab[linha + 1][coluna] = "Y"
+
+
+                    if tab[linha][coluna - 1] == BOLA_PRETA or coluna == 1:
+                        count2 -= 1
+
+                    else:
+                        if tab[linha][coluna - 1] == BOLA_BRANCA:
+                            count2 += 1
+                        else:
+                            tab[linha][coluna - 1] = "Y"
+
+                    if tab[linha][coluna + 1] == BOLA_PRETA or coluna == len(tab):
+                        count2 -= 1
+
+                    else:
+                        if tab[linha][coluna + 1] == BOLA_BRANCA:
+                            count2 += 1
+                        else:
+                            tab[linha][coluna + 1] = "Y"
+
+
+def controi_tabuleiro(TAM):
+    """construir o tabuleiro de Go para iniciar"""
+
+    tab = []
+    for linhas in range(TAM):
+        coluna = []
+        tab.append(coluna)
+        if linhas >= 9:
+            coluna.append("{} |".format(linhas + 1))
+        else:
+            coluna.append("{}  |".format(linhas + 1))
+        for colunas in range(TAM):
+            if colunas == TAM-1:
+                coluna.append("  .")
+                coluna.append(" |")
+            else:
+                coluna.append(" .")
+
+    return tab
+
+
+def imprime_matriz(m, tam):
+    """
+    Imprime um tabuleiro
+    :param m: o tabuleiro a ser impresso
+    """
+    if tam == 9:
+        print("      A  B  C  D  E  F  G  H  I")
+    elif tam == 13:
+        print("      A  B  C  D  E  F  G  H  I  J  K  L  M ")
+    elif tam == 19:
+        print("      A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S ")
+
+    for linha in range(len(m)):
+        for coluna in range(len(m[linha])):
+            if coluna == tam - 1:
+                print(m[linha][coluna], end="")
+            else:
+                print(m[linha][coluna], end=" ")
+        print()
+
+
+def movimentando_tab(linha, coluna, tab, JOG, BOLA_BRANCA, BOLA_PRETA):
+    """movimentação de Go na linha e na coluna que foi selecionado as peças"""
+
+    if JOG == 1:
+        tab[linha][coluna] = BOLA_PRETA
+
+    elif JOG == 2:
+        tab[linha][coluna] = BOLA_BRANCA
+
+
+def interpretando_c(c):
+    """interpretando as entradas de coluna que vão de A a S e substuindo por número interiro"""
+
+    for i in range(len(letras_minusculas)):
+        if c == letras_minusculas[i]:
+            c = i + 1
+            return c
+
+
+def capturando_innimigo(tab):
+
+    for linha in range(len(tab)):
+        cima = False
+        baixo = False
+        esqueda = False
+        direita = False
+
+        for coluna in range(len(tab)):
+            if tab[linha][coluna] == BOLA_PRETA or tab[linha][coluna] == BOLA_BRANCA:
+
+                if tab[linha][coluna] == BOLA_PRETA:
+
+                    if tab[linha - 1][coluna] == BOLA_BRANCA or linha == 0:
+                        cima = True
+                    if linha == len(tab) - 1:
+                        baixo = True
+                    else:
+                        if tab[linha + 1][coluna] == BOLA_BRANCA or linha == len(tab):
+                            baixo = True
+                    if tab[linha][coluna - 1] == BOLA_BRANCA or coluna == 1:
+                        esqueda = True
+                    if tab[linha][coluna + 1] == BOLA_BRANCA or coluna == len(tab):
+                        direita = True
+                    if cima == True and baixo == True and esqueda == True and direita == True:
+                        print("Achou Preta!!")
+                        break
+                else:
+                    if tab[linha-1][coluna] == BOLA_PRETA or linha == 0:
+                        cima = True
+                    if linha == len(tab) - 1:
+                        baixo = True
+                    else:
+                        if tab[linha + 1][coluna] == BOLA_BRANCA or linha == len(tab):
+                            baixo = True
+                    if tab[linha][coluna-1] == BOLA_PRETA or coluna == 1:
+                        esqueda = True
+                    if tab[linha][coluna+1] == BOLA_PRETA or coluna == len(tab):
+                        direita = True
+                    if cima == True and baixo == True and esqueda == True and direita == True:
+                        print("Achou Branca!!")
+                        break
---
+++ ui.py
@@ -0,0 +1,77 @@
+from jogo import *
+from constantes import *
+
+
+def tamanho_tabuleiro():
+    print("Tamanho 9x9 {} Tamanho 13x13 {}\n Tamanho 19x19 {}".format(tab9, tab13, tab19))
+    print()
+    tamnaho_tabuleiro = input("Qual o tamanho que gotariam de jogar 9x9(9) ou 13x13(13) ou 19x19(19)? ")
+    if tamnaho_tabuleiro != "9" and tamnaho_tabuleiro != "13" and tamnaho_tabuleiro != "19":
+        print("Tamanho de tabuleiro errado!, por favor inserir tamanho de tabeleiro dentro das 3 opções")
+        return tamanho_tabuleiro()
+
+    else:
+        TAM = int(tamnaho_tabuleiro)
+        return TAM
+
+
+def chamando_coluna_linha(tam, tab):
+    """Nessa função iremos pedir a coluna e a linha iremos verificar se podemos fazer a jogada
+    e se está dentro dos parametros"""
+
+    letras = ["A", "B", "C",  "D",  "E ",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "R", "S"]
+    numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
+
+    coluna = input("Qual coluna de A a {}? ".format(letras[tam - 1])).lower()
+
+    if coluna not in letras_minusculas:
+        print("Fora do parametro")
+        imprime_matriz(e, tam)
+        return chamando_coluna_linha(tam, tab)
+
+    pego_linha = input("Qual linha de 1 a {}? ".format(tam))
+
+    if pego_linha not in numeros:
+        print("Tem que ser numeros positivos entre 1 e {}".format(tam))
+        print("Ou ser Inteiro!")
+        imprime_matriz(e, tam)
+        return chamando_coluna_linha(tam, tab)
+    linha = int(pego_linha)
+    inter = interpretando_c(coluna)
+
+    if inter > tam or linha > tam or inter < 0 or linha < 0:
+        print("Posição fora do tabuleiro")
+        print("Escolha outra posição")
+        imprime_matriz(e, tam)
+        return chamando_coluna_linha(tam, tab)
+    if tab[linha - 1][inter] == BOLA_PRETA or tab[linha - 1][inter] == BOLA_BRANCA:
+        print("Já tem uma peça nessa posição!")
+        print(" Por favor escolher outra posição")
+        imprime_matriz(e, tam)
+        return chamando_coluna_linha(tam, tab)
+
+    else:
+        return inter, linha
+
+
+
+# Inicia o jogo
+TAM = tamanho_tabuleiro()
+e = controi_tabuleiro(TAM)
+imprime_matriz(e, TAM)
+
+# Laço principal
+while True:
+    print("Vez de jogador 1!")
+    coluna1, linha1 = chamando_coluna_linha(TAM, e)
+    movimentando_tab(linha1-1, coluna1, e, JOG1, BOLA_BRANCA, BOLA_PRETA)
+    contar_liberdades(e)
+    capturando_innimigo(e)
+    imprime_matriz(e, TAM)
+
+    print("Vez de jogador 2!")
+    coluna2, linha2 = chamando_coluna_linha(TAM, e)
+    movimentando_tab(linha2-1, coluna2, e, JOG2, BOLA_BRANCA, BOLA_PRETA)
+    contar_liberdades(e)
+    capturando_innimigo(e)
+    imprime_matriz(e, TAM)
