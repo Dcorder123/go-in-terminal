b24de91d87fa9164a6779b3bfb7c022ecc9ee7d3
Dcorder123 <101361658+Dcorder123@users.noreply.github.com>
2022-03-21 16:59:01 -0300

    Add files via upload

--- constantes.py
+++ constantes.py
@@ -1,10 +1,11 @@
-BOLA_PRETA = " ◯"
-BOLA_BRANCA = " ●"
-VAZIO = "  ."
+PECA_PRETA = " ◯"
+PECA_BRANCA = " ●"
+VAZIO = " ."
 JOG1 = 1
 JOG2 = 2
 liberdade = 8
 letras_minusculas = ["a", "b", "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q", "r", "s"]
+
 
 tab9 = """
       A  B  C  D  E  F  G  H  I
@@ -56,3 +57,5 @@ tab19 = """
 17 |  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  | 
 18 |  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  | 
 19 |  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  | """
+
+
